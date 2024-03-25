00e5d487526e98d064d993bf2451f739de9b533b
Andri10dev <andri10.dev@gmail.com>
2024-03-25 16:33:25 +0700

    disable esline in about section (Landing Page)

--- src/views/langing_page/sections/about_us.jsx
+++ src/views/langing_page/sections/about_us.jsx
@@ -1,22 +1,25 @@
-import AboutImage from '../../../assets/images/about-img.jpg'
-import Title from '../../../components/title'
+import AboutImage from "../../../assets/images/about-img.jpg";
+import Title from "../../../components/title";
 
 const AboutUs = () => {
-    return (
-        <div id="aboutUs" className="w-full bg-custom-blue py-16 -mt-2">
-            <Title name="About Us" color="white"></Title>
-            <div className="flex flex-col md:flex-row items-center mt-10 px-5 md:px-0">
-                <div className="md:w-1/2 md:pl-10">
-                    <img src={AboutImage} alt="linqkeun - about image" className="w-[90%] md:w-[70%] aspect-video rounded-2xl mx-auto" />
-                </div>
-                <div className=" md:w-1/2 md:pr-10 mt-5 md:mt-0">
-                    <p className="text-justify text-white text-sm">
-                    Linqkeun is a mini ERP that is designed specifically for small businesses. It is everything you need to run your business more efficiently and effectively, all in one place
-                    </p>
-                </div>
-            </div>
+  return (
+    <div id="aboutUs" className="w-full bg-custom-blue py-16 -mt-2">
+      <Title name="About Us" color="white"></Title>
+      <div className="flex flex-col md:flex-row items-center mt-10 px-5 md:px-0">
+        <div className="md:w-1/2 md:pl-10">
+          {/*  eslint-disable-next-line */}
+          <img src={AboutImage} alt="linqkeun - about image" className="w-[90%] md:w-[70%] aspect-video rounded-2xl mx-auto" />
         </div>
-    )
-}
+        <div className=" md:w-1/2 md:pr-10 mt-5 md:mt-0">
+          <p className="text-justify text-white text-sm">
+            Linqkeun is a mini ERP that is designed specifically for small
+            businesses. It is everything you need to run your business more
+            efficiently and effectively, all in one place
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default AboutUs
+export default AboutUs;
