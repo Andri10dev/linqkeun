65af3828d5a24ae1492ed916730bf00ab0b15736
Andri10dev <andri10.dev@gmail.com>
2024-03-25 16:28:20 +0700

    fix bug in about section (Landing Page)

--- src/views/langing_page/sections/about_us.jsx
+++ src/views/langing_page/sections/about_us.jsx
@@ -7,7 +7,7 @@ const AboutUs = () => {
             <Title name="About Us" color="white"></Title>
             <div className="flex flex-col md:flex-row items-center mt-10 px-5 md:px-0">
                 <div className="md:w-1/2 md:pl-10">
-                    <image src={AboutImage} alt="linqkeun - about image" className="w-[90%] md:w-[70%] aspect-video rounded-2xl mx-auto" />
+                    <img src={AboutImage} alt="linqkeun - about image" className="w-[90%] md:w-[70%] aspect-video rounded-2xl mx-auto" />
                 </div>
                 <div className=" md:w-1/2 md:pr-10 mt-5 md:mt-0">
                     <p className="text-justify text-white text-sm">
